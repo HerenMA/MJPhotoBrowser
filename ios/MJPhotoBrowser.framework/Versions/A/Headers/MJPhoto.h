f497bc104a0693d54f8623d82641efaad44569ad
yinjianjun <yinjianjun1@herenit.com>
2024-03-15 09:40:56 +0800

    bumping the version to 1.0.6

---
+++ ios/MJPhotoBrowser.framework/Versions/A/Headers/MJPhoto.h
@@ -0,0 +1,26 @@
+//
+//  MJPhoto.h
+//
+//  Created by mj on 13-3-4.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+
+#import <YYWebImage/YYWebImage.h>
+
+@interface MJPhoto : NSObject
+
+/// 图片URL
+@property (strong, nonatomic) NSURL *url;
+/// 视频URL
+@property (strong, nonatomic) NSURL *videoUrl;
+/// 完整的图片
+@property (strong, nonatomic) UIImage *image;
+/// 默认图片
+@property (strong, readonly, nonatomic) UIImage *placeholder;
+/// 索引
+@property (assign, nonatomic) NSUInteger index;
+
+@end
